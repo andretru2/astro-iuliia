6948f41fde2b7dd96d4f150d28d73df79f7e98d0
Andres Trujillo <andretru2@gmail.com>
2022-12-29 19:50:26 -0500

    added astrojs/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), sitemap(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
